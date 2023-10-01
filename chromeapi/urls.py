98a821e65d15ea99aef70703ab274c08289a6d81
njokuchidinma <njokuchidinma16@gmail.com>
2023-10-01 22:33:34 +0100

    final lap done...

--- chromeapi/urls.py
+++ chromeapi/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,5 +24,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-] 
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
